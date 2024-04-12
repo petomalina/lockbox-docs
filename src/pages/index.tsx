39f46d5bd3e8d9ebf6744b5f15a0f9806456f629
Peter Malina <peter.malina@kiwi.com>
2024-04-12 10:19:40 +0000

    feat: new structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/quickstart' />;
+  return <Redirect to='/tutorial/quickstart' />;
 }
